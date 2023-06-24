bb20e7ad2b3e8e879c3085bfa90bd016b3cccd02
xofaa <giovanne.oliv@gmail.com>
2023-06-23 22:30:55 -0300

    correcao no update de itensadicionais

--- controllers/ItensAdicionaisController.js
+++ controllers/ItensAdicionaisController.js
@@ -1,130 +1,132 @@
-const {ItemAdicional,ProdutoItemAdicional}=require('../models')
+const { ItemAdicional, ProdutoItemAdicional } = require('../models')
 
 
 
-class ItensAdicionaisController{
-    
-    static async getItens(req,res){
 
-        try {
-        const itens=await ItemAdicional.findAll()
-        res.status(200).json({
-            data:itens
-        })
+class ItensAdicionaisController {
 
-        } catch (error) {
-            res.status(400).json({
-                error:true,
-                message:error.message
-            })
-        }
-    }
-
-    static async createItem(req,res){
+  static async getItens(req, res) {
 
     try {
-        const itens = await ItemAdicional.findOne({
-            where:{
-                titulo:req.body.titulo
-            }
-        })
-        if(!itens){
+      const itens = await ItemAdicional.findAll()
+      res.status(200).json({
+        data: itens
+      })
+
+    } catch (error) {
+      res.status(400).json({
+        error: true,
+        message: error.message
+      })
+    }
+  }
+
+  static async createItem(req, res) {
+
+    try {
+      const itens = await ItemAdicional.findOne({
+        where: {
+          titulo: req.body.titulo
+        }
+      })
+      if (!itens) {
         await ItemAdicional.create({
-            titulo:req.body.titulo,
-            descricao:req.body.descricao,
-            valor:parseFloat(req.body.valor)
+          titulo: req.body.titulo,
+          descricao: req.body.descricao,
+          valor: parseFloat(req.body.valor)
         })
         res.status(200).json({
-            message:'cadastro feito com sucesso'
+          message: 'cadastro feito com sucesso'
         })
-        }else{
-            res.status(500).json({
-                message:'item Adicional já existe'
-            })
-        }
+      } else {
+        res.status(500).json({
+          message: 'item Adicional já existe'
+        })
+      }
     } catch (error) {
-        res.status(400).json({
-            error:true,
-            message:error.message
-        })
+      res.status(400).json({
+        error: true,
+        message: error.message
+      })
     }
-    }
+  }
 
-    static async updateItem(req,res){
-        const itens = await ItemAdicional.findOne({
-            where:{
-                titulo:req.body.titulo
-            }
-        })
-        const novoItem=await ItemAdicional.findByPk(req.params.id)
-        try {
-            if(itens && novoItem){
-                res.status(501).json({
-                    message:'já existe um item com esse nome'
-                })
-            }
-            else{
-                if(novoItem){
-                    await novoItem.update({
-                        titulo:req.body.titulo,
-                        descricao:req.body.descricao,
-                        valor:parseFloat(req.body.valor)
-                    })
-                    res.status(200).json({
-                        message:'item alterado com sucesso'
-                    })
-                }else{
-                    res.status(500).json({
-                        message:'item não encontrado'
-                    })
-                }
-               
-            }
+  static async updateItem(req, res) {
+    try {
+      const itens = await ItemAdicional.findOne({
+        where: {
+          titulo: req.body.titulo,
 
-        } catch (error) {
-
-            res.status(400).json({
-                error:true,
-                message:error.message
-            })
         }
-    }
-    static async deleteItem(req, res) {
-        try {
-          const itemAdicionalId = req.params.id;
-      
-          // Verifica se o item adicional existe
-          const itemAdicional = await ItemAdicional.findByPk(itemAdicionalId);
-      
-          if (!itemAdicional) {
-            return res.status(404).json({
-              error: true,
-              message: 'Item adicional não encontrado.'
-            });
-          }
-      
-          // Exclui os registros relacionados na tabela de junção (produtoitemadicional)
-          await ProdutoItemAdicional.destroy({
-            where: {
-              itemAdicionalId: itemAdicionalId
-            }
+      });
+
+      const novoItem = await ItemAdicional.findByPk(req.params.id);
+
+      if (itens && novoItem) {
+        res.status(501).json({
+          message: 'Já existe um item com esse nome'
+        });
+      } else {
+        if (!novoItem) {
+          res.status(500).json({
+            message: 'Item não encontrado'
           });
-      
-          // Exclui o registro do item adicional
-          await itemAdicional.destroy();
-      
-          return res.status(200).json({
-            message: 'Item adicional deletado com sucesso.'
+        } else {
+          await novoItem.update({
+            titulo: req.body.titulo,
+            descricao: req.body.descricao,
+            valor: parseFloat(req.body.valor)
           });
-        } catch (error) {
-          return res.status(500).json({
-            error: true,
-            message: error.message
+          res.status(200).json({
+            message: 'Item alterado com sucesso'
           });
         }
       }
-      
-      
+    } catch (error) {
+      res.status(400).json({
+        error: true,
+        message: error.message
+      });
+    }
+  }
+
+
+  static async deleteItem(req, res) {
+    try {
+      const itemAdicionalId = req.params.id;
+
+      // Verifica se o item adicional existe
+      const itemAdicional = await ItemAdicional.findByPk(itemAdicionalId);
+
+      if (!itemAdicional) {
+        return res.status(404).json({
+          error: true,
+          message: 'Item adicional não encontrado.'
+        });
+      }
+
+      // Exclui os registros relacionados na tabela de junção (produtoitemadicional)
+      await ProdutoItemAdicional.destroy({
+        where: {
+          itemAdicionalId: itemAdicionalId
+        }
+      });
+
+      // Exclui o registro do item adicional
+      await itemAdicional.destroy();
+
+      return res.status(200).json({
+        message: 'Item adicional deletado com sucesso.'
+      });
+    } catch (error) {
+      return res.status(500).json({
+        error: true,
+        message: error.message
+      });
+    }
+  }
+
+
 
 }
-module.exports=ItensAdicionaisController
+module.exports = ItensAdicionaisController
